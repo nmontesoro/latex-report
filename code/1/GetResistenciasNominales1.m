c1fcd1ea55f890b39d5616e71b109d75c6a96c5e
Nazareno Montesoro <n.montesoro@gmail.com>
2022-10-27 13:25:01 -0300

    Corrijo columnas

--- code/1/GetResistenciasNominales1.m
+++ code/1/GetResistenciasNominales1.m
@@ -1,6 +1,6 @@
 function [R, DR] = GetResistenciasNominales1()
     filename = "data/1/mediciones-parsed.csv";
 
-    R = GetDataColumns(filename, [1, 3, 5, 7]);
-    DR = GetDataColumns(filename, [2, 4, 6, 8]);
+    R = GetDataColumns(filename, [3, 5, 7, 9]);
+    DR = GetDataColumns(filename, [4, 6, 8, 10]);
 end
