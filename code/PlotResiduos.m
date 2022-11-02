f09b5f652c09a6df3aa3543df06a2b6cffcf695b
Nazareno Montesoro <n.montesoro@gmail.com>
2022-11-02 19:15:05 -0300

    Agrego unidades a los ejes

--- code/PlotResiduos.m
+++ code/PlotResiduos.m
@@ -1,6 +1,6 @@
 [X, Y] = GetResiduos();
 scatter(X, Y, "filled");
 grid on;
-xlabel("$\\sqrt{L}$");
-ylabel("$T$ (\\si{\\second})");
+xlabel("$\\sqrt{L}$ (\\si{\\centi\\meter\\tothe{1/2}})");
+ylabel("$T - \\hat{T}$ (\\si{\\second})");
 MyPrint("img/residuos.tikz");
