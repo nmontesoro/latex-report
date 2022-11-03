985384462af8da831c851f94ee021b30bd053c47
Nazareno Montesoro <n.montesoro@gmail.com>
2022-11-03 09:21:53 -0300

    Quito barras de error

--- code/MakeBasePlot.m
+++ code/MakeBasePlot.m
@@ -13,7 +13,8 @@ function [data, leg] = MakeBasePlot(root)
     for i = 1:m
         j = N * i - (N - 1);
         k = j + N - 1;
-        errorbar(data(j:k, 1), data(j:k, 3), data(j:k, 2), data(j:k, 4), "~>.");
+        % errorbar(data(j:k, 1), data(j:k, 3), data(j:k, 2), data(j:k, 4), "~>.");
+        scatter(data(j:k, 1), data(j:k, 3), "filled");
         leg(i, :) = sprintf("$L = \\SI{%f \\pm %f}{\\centi\\meter}$", L(j), DL(j));
     end
     grid on;
